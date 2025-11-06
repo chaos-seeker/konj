aee996447749aba7e8a326f50c81ea4d6d2feda1
hamid shahsavani <051.sys113@gmail.com>
2025-11-06 14:18:05 +0330

    chore: new changes

--- containers/routes/home/lowest.tsx
+++ containers/routes/home/lowest.tsx
@@ -1,5 +1,6 @@
 import { ProductCart } from "@/components/product-cart";
 import Link from "next/link";
+import Image from "next/image";
 import { ArrowLeftIcon } from "lucide-react";
 import { getBooks } from "@/actions/dashboard/manage-books/get-books";
 
@@ -28,23 +29,37 @@ export const Lowest = async () => {
             <ArrowLeftIcon className="size-4 group-hover:text-primary shrink-0" />
           </Link>
         </div>
-        <div className="lg:hidden overflow-x-auto snap-x snap-mandatory">
-          <div className="flex gap-4">
-            {sorted.map((item) => (
-              <div
-                key={item.id}
-                className="min-w-[200px] max-w-[200px] snap-start shrink-0"
-              >
-                <ProductCart book={item} />
-              </div>
-            ))}
+        {sorted.length === 0 ? (
+          <div className="items-center flex flex-col gap-6 justify-center py-10">
+            <Image
+              src="/images/global/not-found.png"
+              alt="empty"
+              width={200}
+              height={200}
+            />
+            <p className="text-muted-foreground">کتابی برای نمایش وجود ندارد</p>
           </div>
-        </div>
-        <div className="hidden lg:grid grid-cols-6 gap-4">
-          {sorted.slice(0, 6).map((item) => (
-            <ProductCart key={item.id} book={item} />
-          ))}
-        </div>
+        ) : (
+          <>
+            <div className="lg:hidden overflow-x-auto snap-x snap-mandatory">
+              <div className="flex gap-4">
+                {sorted.map((item) => (
+                  <div
+                    key={item.id}
+                    className="min-w-[200px] max-w-[200px] snap-start shrink-0"
+                  >
+                    <ProductCart book={item} />
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="hidden lg:grid grid-cols-6 gap-4">
+              {sorted.slice(0, 6).map((item) => (
+                <ProductCart key={item.id} book={item} />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
